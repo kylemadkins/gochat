168a6c3e390aa404eea118908696b9da385ce50a
kylemadkins <kyle.m.adkins@gmail.com>
2023-07-29 00:52:04 -0400

    Basic WebSocket endpoint and client

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 var views = jet.NewSet(
@@ -12,8 +13,46 @@ var views = jet.NewSet(
 	jet.InDevelopmentMode(),
 )
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		accepted := []string{"http://localhost:8000"}
+		for _, v := range accepted {
+			if v == r.Header["Origin"][0] {
+				return true
+			}
+		}
+		return false
+	},
+}
+
+type WsResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+type WsPayload struct {
+	Username string          `json:"username"`
+	Action   string          `json:"action"`
+	Message  string          `json:"message"`
+	Conn     *websocket.Conn `json:"-"`
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "index.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func Ws(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Client connected")
+	resp := WsResponse{Message: "Connected to server!"}
+	err = ws.WriteJSON(resp)
 	if err != nil {
 		log.Println(err)
 	}
